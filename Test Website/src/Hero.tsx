10efdf4000e0b79efa78df145937e5763228ceaf
Alexie-Campbell <alexie.campbell@mymona.uwi.edu>
2024-09-03 22:35:26 -0500

    first react webpage

---
+++ Test Website/src/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Hero.css';
+
+
+const Hero: React.FC = () => {
+    return(
+        <section className='hero'>
+            <div className='content'>
+                <h1 className='title'> Test App </h1>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
---
+++ Test Website/src/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navbar.css'
+
+const Navbar: React.FC = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href = "#home">Home</a>
+                </li>
+                <li>
+                    <a href = "#about">About</a>
+                </li>
+                <li>
+                    <a href = "#contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
